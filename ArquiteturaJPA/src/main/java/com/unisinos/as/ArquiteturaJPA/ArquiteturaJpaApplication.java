eb515c783d5eb13733f797b8bc536bcd42abe6db
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2020-09-18 06:50:08 -0300

    Inicialização do Projeto Spring Boot Maven
    Dependências:
    H2
    JPA
    Web
    JDBC

---
+++ ArquiteturaJPA/src/main/java/com/unisinos/as/ArquiteturaJPA/ArquiteturaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.unisinos.as.ArquiteturaJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaJpaApplication.class, args);
+	}
+
+}
---
+++ ArquiteturaJPA/src/test/java/com/unisinos/as/ArquiteturaJPA/ArquiteturaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisinos.as.ArquiteturaJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
